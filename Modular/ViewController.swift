83378ee07113e7361d0e5981b857dae5327bc102
somsakGitHub <somsak.kaeworasan@outlook.com>
2023-08-21 18:07:00 +0700

    Init project.

---
+++ Modular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modular
+//
+//  Created by somsak on 21/8/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
